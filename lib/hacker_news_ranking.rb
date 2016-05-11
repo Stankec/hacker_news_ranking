ce6500c39e6de515f3fc6813f8a0895309a7be88
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-05-11 22:45:58 +0200

    Fix block passing

--- lib/hacker_news_ranking.rb
+++ lib/hacker_news_ranking.rb
@@ -1,5 +1,18 @@
-require "hacker_news_ranking/version"
+require 'hacker_news_ranking/version'
+require 'hacker_news_ranking/configuration'
 
-module HackerNewsRanking
-  # Your code goes here...
+class HackerNewsRanking
+  def self.configure(&block)
+    Configuration.new(&block)
+    nil
+  end
+
+  def self.rank(array:, points:, timestamp:)
+  end
+
+  def self.rank!(array:, points:, timstamp:)
+  end
+
+  def initialize(options = {})
+  end
 end
