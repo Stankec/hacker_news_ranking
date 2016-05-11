6a25d45236bcd8ec75f79373b3eea43b5c1c4826
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-05-11 22:46:57 +0200

    Create test for configurator

--- spec/hacker_news_ranking_spec.rb
+++ spec/hacker_news_ranking_spec.rb
@@ -4,4 +4,44 @@ describe HackerNewsRanking do
   it 'has a version number' do
     expect(HackerNewsRanking::VERSION).not_to be nil
   end
+
+  context 'class method' do
+    describe '#configure' do
+      let(:configuration) { HackerNewsRanking::Configuration.configuration }
+
+      it 'returns defaults' do
+        expect(configuration).to(
+          include(
+            timestamp: :created_at,
+            gravity: 1.8,
+            scope_method: :trending,
+            current_rank_method: :rank
+          )
+        )
+      end
+
+      it 'saves the passed configuration' do
+        HackerNewsRanking.configure do
+          points :comments_count
+          timestamp :commented_at
+          gravity 2.3
+          scope_method :controversial
+          current_rank_method :points
+        end
+
+        expect(configuration).to(
+          include(
+            points: :comments_count,
+            timestamp: :commented_at,
+            gravity: 2.3,
+            scope_method: :controversial,
+            current_rank_method: :points
+          )
+        )
+      end
+    end
+  end
+
+  context 'instance method' do
+  end
 end
