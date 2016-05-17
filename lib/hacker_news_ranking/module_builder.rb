b1facda897983d563484514ec25e58f2dee1252e
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-05-17 21:42:52 +0200

    Add adapter lookup

--- lib/hacker_news_ranking/module_builder.rb
+++ lib/hacker_news_ranking/module_builder.rb
@@ -15,8 +15,25 @@ class HackerNewsRanking
     private
 
     def adapter
-      # TODO: implement logict that racefuuly checks model class without
-      # requiring active record or sequel
+      if sequel_defined? && klass.is_a?(Sequel::Model)
+        Adapter::Sequel
+      elsif active_record_defined? && klass.is_a?(ActiveRecord::Base)
+        Adapter::ActiveRecord
+      end
+    end
+
+    def sequel_defined?
+      !class_from_string('Sequel::Model').nil?
+    end
+
+    def active_record_defined?
+      !class_from_string('ActiveRecord::Base').nil?
+    end
+
+    def class_from_string(class_name)
+      Object.const_get(class_name)
+    rescue NameError
+      nil
     end
   end
 end
